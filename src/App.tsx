81e5e564ac4a2b26bc6523ce24ec9a1e3303a222
Kolya <86050654+KolyaTeslya@users.noreply.github.com>
2023-03-25 00:56:54 +0200

    add delete tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { TaskType, Todolist } from "./Todolist";
 
+
 function App() {
 
-  let tasks: Array<TaskType>  = [
+  let initTasks: Array<TaskType>  = [
     { id: 1, title: "CSS", isDone: true },
     { id: 2, title: "JS", isDone: false },
     { id: 3, title: "ReactJS", isDone: true },
     { id: 4, title: "Redux", isDone: false },
-
   ]
+
+  let arr = useState(initTasks)
+
+  let tasks = arr[0]
+  let setTasks = arr[1]
+
+
+  function removeTask(id: number) {
+    let filteredTasks = tasks.filter(t =>  t.id !== id)
+    setTasks(filteredTasks);
+  }
 
   return (
     <div className="App">
-      <Todolist title="What to learn?" tasks={tasks} />
+      <Todolist title="What to learn?" 
+                tasks={tasks} 
+                removeTask={removeTask}
+      />
     </div>
   );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -14,7 +14,8 @@ export type TaskType = {
 
 type PropsType = {
   title: string,
-  tasks: Array<TaskType>
+  tasks: Array<TaskType>,
+  removeTask: Function
 }
 
 export function Todolist(props: PropsType) {
@@ -30,7 +31,7 @@ export function Todolist(props: PropsType) {
         {
           props.tasks.map(t => <li> <input type="checkbox" checked={t.isDone} />
                         <span>{t.title}</span>
-                        <button onClick={ () => { alert(t.id) } }>x</button>
+                        <button onClick={ () => { props.removeTask(t.id)} }>x</button>
             </li>
 
           )
